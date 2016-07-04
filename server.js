520a8eee1996096a80d79892295ba9dca391cd88
rahmona-henry <rahmona.henry@icloud.com>
2016-07-04 21:50:45 +1200

    QUery wip

--- server.js
+++ server.js
@@ -15,12 +15,26 @@ app.get('/', function(req,res){
   res.send('index.html')
 })
 
+app.get('/stats', function(req,res){
+  res.render('statsRequest')
+})
+
+
+app.get('/stats/:id', function(req,res){
+  knex('customers').where({country:req.body.country}).select('total')
+    .then(function(data){
+          res.render('statsReturned')
+    })
+})
+
+
 app.post('/add', function(req,res){
   knex('customers').where({country:req.body.country}).increment('total',1)
     .then(function(data){
           res.render('thankYou')
     })
 })
+
 
 app.listen(3000, function(){
   console.log('We have lift off on port 3000')
